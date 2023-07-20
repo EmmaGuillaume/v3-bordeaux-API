e4f0ee6875c0aa1c0ed41db5fd96a59e598e15f7
Arthaud <arthaudp33@gmail.com>
2023-07-20 12:51:28 +0200

    fix: build

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,13 +1,13 @@
 'use client'
-import React from 'react'
+import { useEffect } from 'react'
 
 import { useAppSelector } from '@/redux/hooks'
 
 import { H1 } from '@/components/atoms/H1'
-import Logout from '@/components/organisms/Auth/Logout'
 import { Informations } from '@/components/organisms/Account/Informations'
-import { RentInProgress } from '@/components/organisms/Account/RentInProgress'
 import { RentCompleted } from '@/components/organisms/Account/RentCompleted'
+import { RentInProgress } from '@/components/organisms/Account/RentInProgress'
+import Logout from '@/components/organisms/Auth/Logout'
 import { useRouter } from 'next/navigation'
 
 import type { RootState } from '@/redux/store'
@@ -16,9 +16,11 @@ export default function Home() {
   const router = useRouter()
   const token = useAppSelector((state: RootState) => state.cykleoTokenReducer.value)
 
-  if (!token) {
-    router.replace('/login')
-  }
+  useEffect(() => {
+    if (!token) {
+      router.replace('/login')
+    }
+  }, [])
   return (
     <main className="px-4">
       <H1 className="!my-10 text-center">V3 Bordeaux</H1>
